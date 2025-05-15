d47f8cf851dd7308de2793d4503a769afaae1975
Raghulkannan005 <raghulkannan005@gmail.com>
2025-05-15 12:37:19 +0530

    recursion

---
+++ maths/palindromeCheck.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int input) {
+        int x = input;
+        if(x<0) return false;
+        int finalValue = 0;
+        while(x!=0){
+            int temp = x%10;
+            x = x/10;
+            if (finalValue > INT_MAX / 10 || (finalValue == INT_MAX / 10 && temp > 7)) return 0;
+            if (finalValue < INT_MIN / 10 || (finalValue == INT_MIN / 10 && temp < -8)) return 0;
+            finalValue = finalValue * 10 + temp;
+        }
+        if(input == finalValue){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+int main(){
+    int n;
+    cin >> n;
+    cout << n << "is Palindrome : "<< isPalindrome(n)<< endl;
+}
