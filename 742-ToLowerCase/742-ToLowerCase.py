e46efe3c60f52cfd0c7564eb202b65c89cfbf16f
Raghulkannan005 <raghulkannan005@gmail.com>
2025-07-18 19:02:49 +0530

    removed leetcode sync and moved it into seperate folder

--- 742-ToLowerCase/742-ToLowerCase.py
+++
@@ -1,9 +0,0 @@
-# Last updated: 7/18/2025, 6:57:59 PM
-class Solution(object):
-    def toLowerCase(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        return s.lower()
-        
